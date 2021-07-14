33fb0a040e72bba5a7ff86e1dc45d6156b78b7ba
roma8ok <roma8ok@gmail.com>
2021-07-14 11:52:19 +0700

    added microservice to get body from duckduckgo search response and send to queue

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/streadway/amqp"
+)
+
+const (
+	rabbitURL                = "amqp://admin:admin@localhost:5672/"
+	searchQuery              = "proxy list"
+	proxyURL                 = ""
+	userAgent                = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+	queueSearchBodiesFromDDG = "search_bodies_from_DDG"
+)
+
+func main() {
+	conn, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func(conn *amqp.Connection) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}(conn)
+
+	if err := sendSearchBodyFromDDGToQueue(conn, searchQuery, proxyURL, userAgent); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
